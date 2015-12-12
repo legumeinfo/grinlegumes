160b9db807da2908deb279f8a224a23599361b4f
Sudhansu Dash <sdash@lis-dev.agron.iastate.edu>
2015-12-11 18:45:37 -0600

    Major modification to get it ready for paasport and evaluation data with links to GRIN and map.

--- grinlegumes_page.tpl.php
+++ grinlegumes_page.tpl.php
@@ -2,7 +2,7 @@
 <div>
   <h1>GRIN Legumes: Germplasn Data from GRIN for Legumes</h1>
   <span>Search for trait evaluation data with a GRIN accession no for the available species.</span>
-  <span style="font-size: 75%">(Based on the descriptor data downloaded from GRIN in Sep 2015.)</span>
+  <span style="font-size: 75%">(Based on the descriptor data downloaded from GRIN.)</span>
   <hr/>
 </div>
 
@@ -10,11 +10,12 @@
 <div>
 <form  method="get">
   <br/>
-  Enter the Genus and a GRIN Accession Number (<i>without any prefix like 'PI', just the number</i>): <br/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 
+  Enter a GRIN Accession Number and Genus (optional): <br/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 
   
   <input type="text" id="grin_acc_no" name="grin_acc_no" required value="<?php print $_GET['grin_acc_no']; ?>" >
   &nbsp;&nbsp;&nbsp;&nbsp;
   
+   Genus (optional): 
    <select  id="grin_taxon"  name="grin_taxon"  value="<?php print $_GET['grin_taxon']; ?>" >
      <option value="">Any</option>
      <option value="Arachis">Arachis</option>
@@ -29,30 +30,25 @@
 
 </form>
 </div>
-
+<hr/>
 <!-- Message  -->
+<!--
 <div>
   <hr/>
   <fieldset  style="background: #E0E0E0;">
-    <?php  echo "<b>ACCESSION NUMBER: ".$acc_no."</b>";  echo "<br/>"
-    ."Extra for debug: ".$acc_no2."  prefix: ".$acc_no_prefix."  num: ".$acc_no_num; 
+    <?php  echo "<b>ACCESSION NUMBER: ".$acc_no."</b>";  
+    echo "<br/>";
+    //echo "Extra for debug: ".$acc_no2."  prefix: ".$acc_no_prefix."  num: ".$acc_no_num; 
     ?>
   </fieldset>
 </div>
-
-<!-- Iframe GRIN  -->
+-->
+<!-- html -->
 
 <div>
-  <?php  //echo "hello".$result;  ?>
   <?php
-  
-    
     echo $html_content;
-    
-
   ?>
-    
-    
 </div>
 
 
